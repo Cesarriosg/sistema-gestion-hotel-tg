4dc3d6a8586ad29c5eea592638406ac038571cc4
Cesar Rios <rios.cesar@correounivalle.edu.co>
2026-04-07 23:19:45 -0500

    cambios para despliegue

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -5,13 +5,18 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost",
-  database: process.env.DB_NAME || "hotel",
-  password: process.env.DB_PASSWORD || "123456",
-  port: process.env.DB_PORT || 5432,
-});
+export const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.DB_USER || "postgres",
+      host: process.env.DB_HOST || "localhost",
+      database: process.env.DB_NAME || "hotel",
+      password: process.env.DB_PASSWORD || "123456",
+      port: process.env.DB_PORT || 5432,
+    });
 
 export default pool;
 
